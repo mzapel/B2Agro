b706714fd4a76352eb30bf44282d09b270b46ec7
Fabio Nicolau Caceres <fabiorowds@gmail.com>
2019-02-04 20:29:05 -0200

    Capturar imagem.
    Commit para exemplo de funcionamento da camera.

--- B2Agro/B2Agro.Android/MainActivity.cs
+++ B2Agro/B2Agro.Android/MainActivity.cs
@@ -6,20 +6,40 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Media;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
 
 namespace B2Agro.Droid
 {
     [Activity(Label = "B2Agro", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            int requestPermissions = 0;
+            string cameraPermission = Android.Manifest.Permission.Camera;
+
+            if (!(ContextCompat.CheckSelfPermission(this, cameraPermission) == (int)Permission.Granted))
+            {
+                ActivityCompat.RequestPermissions(this, new string[] { cameraPermission }, requestPermissions);
+            }
+
             base.OnCreate(savedInstanceState);
+            
+            await CrossMedia.Current.Initialize();
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
---
+++ B2Agro/B2Agro/Model/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2Agro.Model
+{
+    public class Item
+    {
+        public int ID { get; set; }
+
+    }
+}
